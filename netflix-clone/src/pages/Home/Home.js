311103771301ee7acd64d51f51bab3e2b529a31f
yared tesfaye <etsubtesfaye.w@gmail.com>
2025-09-08 12:24:02 -0700

    row

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import Header from '../../components/Header/Header/Header'
 import Banner from "../../components/Banner/Banner";
 
 import Footer from '../../components/Footer/Footer'
+import RowList from '../../components/Rows/RowList/RowList';
 
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
     <>
      <Header/>
      <Banner/>
+     <RowList/>
      <Footer/>
     </>
   );
